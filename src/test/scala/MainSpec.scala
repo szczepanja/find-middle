45a0936f2e3bc6273b12ee6b3f61f798f2523a9e
a_szczepanik <a.szczepanik@adform.com>
2022-05-31 14:21:42 +0200

    Add basic test

--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,3 +1,17 @@
-class MainSpec {
+import Main._
+import org.scalatest._
+import flatspec._
+import matchers._
+
+class MainSpec extends AnyFlatSpec with should.Matchers {
+
+  behavior of "Main.findMiddleElement"
+
+  it should "find middle element in list" in {
+    findMiddleElement(List(1, 2, 3)) shouldBe Some(2)
+    findMiddleElement(List(1, 4, 6, 7)) shouldBe Some(4)
+    findMiddleElement(List(0, 12, 23, 44, 7, 88, 9)) shouldBe Some(44)
+    findMiddleElement(List(0, 12, 23, 7, 88, 9)) shouldBe Some(23)
+  }
 
 }
