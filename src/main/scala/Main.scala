d50d734d89f0d9f21ae73095275d5e4460e3184c
a_szczepanik <a.szczepanik@adform.com>
2022-05-31 14:21:24 +0200

    Find bug in findMiddleElement method

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+import scala.annotation.tailrec
+
+object Main {
+
+  def findMiddleElement[T](xs: List[T]): Option[T] = {
+    @tailrec
+    def loop(ys: List[T], acc: List[T], cnt: Int): Option[T] = {
+      ys match {
+        case head :: _ if cnt % 2 != 0 => loop(ys.init, acc.init, cnt + 1)
+        case head :: _ => loop(ys.init, acc, cnt + 1)
+        case Nil => acc.reverse.headOption
+      }
+    }
+
+    loop(xs, xs, 0)
+
+  }
+
+}
